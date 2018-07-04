41ac2c69da2a0172e5d4f6b13cbf1f3d2f243986
Marat Tlepberghenov <mur.marat@gmail.com>
2018-07-04 23:40:23 +0300

    Creates class which initializes data base and extends Application for initialize room first

---
+++ app/src/main/java/com/tmarat/retfofitandroom/App.java
@@ -0,0 +1,26 @@
+package com.tmarat.retfofitandroom;
+
+import android.app.Application;
+import android.arch.persistence.room.Room;
+import com.tmarat.retfofitandroom.model.database.WeatherDataBase;
+
+public class App extends Application {
+
+  public static App instance;
+
+  private WeatherDataBase dataBase;
+
+  @Override public void onCreate() {
+    super.onCreate();
+    instance = this;
+    dataBase = Room.databaseBuilder(this, WeatherDataBase.class, "weather").build();
+  }
+
+  public static App getInstance() {
+    return instance;
+  }
+
+  public WeatherDataBase getDataBase() {
+    return dataBase;
+  }
+}
